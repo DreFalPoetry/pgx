3118346ceeaab2e4db37dfa9d3b4cff47618801a
liuranran <864749024@qq.com>
2021-01-14 09:21:47 +0800

    feat: 切换路由方式

---
+++ pgx/src/components/Nav/TopNav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Menu } from 'antd';
+
+export default class TopNav extends React.Component{
+  render(){
+    return (
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        style={{ height: '100%', borderRight: 0 }}
+      >
+        <Menu.Item key="1">option1</Menu.Item>
+        <Menu.Item key="2">option2</Menu.Item>
+        <Menu.Item key="3">option3</Menu.Item>
+        <Menu.Item key="4">option4</Menu.Item>
+      </Menu>
+    )
+  }
+}
