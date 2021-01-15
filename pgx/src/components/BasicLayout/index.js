57cff0203644d15bc1348778df5ff6c2d0885ccc
wangcx <wangchunxiang@f-shark.net>
2021-01-15 17:42:39 +0800

    主页面样式修改

--- pgx/src/components/BasicLayout/index.js
+++ pgx/src/components/BasicLayout/index.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom'
 import { Layout, Menu, Breadcrumb } from 'antd';
 import SideNav from '../Nav/SideNav'
 import TopNav from '../Nav/TopNav'
+import './index.css'
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
@@ -13,32 +14,32 @@ const router = [
     children: [
       {
         title: '总览',
-        icon: 'bar-chart',
+        icon: 'sidebar-icon-dashboard',
         key: '/pgs/dashboard',
       },
       {
         title: '报告列表',
-        icon: 'snippets',
+        icon: 'sidebar-icon-report',
         key: '/pgs/report',
       },
       {
         title: '数据管理',
-        icon: 'snippets',
+        icon: 'sidebar-icon-data_manage',
         key: '/pgs/data_manage',
       },
       {
         title: '统计',
-        icon: 'dot-chart',
+        icon: 'sidebar-icon-statistics',
         key: '/pgs/statistics',
       },
       {
         title: '数据回顾',
-        icon: 'snippets',
+        icon: 'sidebar-icon-statistics',
         key: '/pgs/data_statistics',
       },
       {
         title: '回收站',
-        icon: 'delete',
+        icon: 'sidebar-icon-recycle_bin',
         key: '/pgs/recycle_bin',
       }
     ]
@@ -49,22 +50,22 @@ const router = [
     children: [
       {
         title: '总览',
-        icon: 'bar-chart',
+        icon: 'sidebar-icon-dashboard',
         key: '/pgd/dashboard',
       },
       {
         title: '报告列表',
-        icon: 'snippets',
+        icon: 'sidebar-icon-report',
         key: '/pgd/report',
       },
       {
         title: '数据管理',
-        icon: 'snippets',
+        icon: 'sidebar-icon-data_manage',
         key: '/pgd/data_manage',
       },
       {
         title: '统计',
-        icon: 'dot-chart',
+        icon: 'sidebar-icon-statistics',
         key: '/pgd/statistics',
       },
       // {
@@ -74,7 +75,7 @@ const router = [
       // },
       {
         title: '回收站',
-        icon: 'delete',
+        icon: 'sidebar-icon-recycle_bin',
         key: '/pgd/recycle_bin',
       }
     ]
@@ -85,22 +86,22 @@ const router = [
     children: [
       {
         title: '总览',
-        icon: 'bar-chart',
+        icon: 'sidebar-icon-dashboard',
         key: '/azf/dashboard',
       },
       {
         title: '报告列表',
-        icon: 'snippets',
+        icon: 'sidebar-icon-report',
         key: '/azf/report',
       },
       {
         title: '数据管理',
-        icon: 'snippets',
+        icon: 'sidebar-icon-data_manage',
         key: '/azf/data_manage',
       },
       {
         title: '统计',
-        icon: 'dot-chart',
+        icon: 'sidebar-icon-statistics',
         key: '/azf/statistics',
       },
       // {
@@ -110,7 +111,7 @@ const router = [
       // },
       {
         title: '回收站',
-        icon: 'delete',
+        icon: 'sidebar-icon-recycle_bin',
         key: '/azf/recycle_bin',
       }
     ]
@@ -122,15 +123,28 @@ class BasicLayout extends React.Component{
     super(props)
     this.state = {
       mainMenuKey:'',
-      sideMenuArr:[]
+      sideMenuArr:[],
+      title:''
     }
   }
 
   componentDidMount(){
-    // console.log(this.props.location.pathname)
+  
     const currentPath = this.props.location.pathname
     const _currentMainMenu = '/' + currentPath.split('/').filter(v=>v)[0]
     this.setCurrentMenu(_currentMainMenu)
+   
+    router.map(item=>{
+      item.children&&item.children.map(item=>{
+        if(item.key==currentPath){
+          this.setState({
+            title:item.title
+          })
+        }
+      })
+      
+     
+    })
   }
 
   changeSideMenu = (curNav) => {
@@ -143,48 +157,50 @@ class BasicLayout extends React.Component{
     router.map(v=>{
       if(v.key == curNav){
         _currentSideMenu = v.children
+        
       }
     })
     this.setState({
       mainMenuKey: curNav,
-      sideMenuArr: _currentSideMenu
+      sideMenuArr: _currentSideMenu,
+      curKey:curNav+new Date().getTime()
     })
   }
-
+// 获取面包屑
+  onSelected=(val)=>{
+      this.setState({
+        title:val
+      })
+    }
   render(){
-    const  {mainMenuKey, sideMenuArr} = this.state
+    const  {mainMenuKey, sideMenuArr,curKey,title} = this.state
     return (
       <Layout>
         <Header className="header">
           <div className="logo" />
           <TopNav 
-            key={mainMenuKey}
+            key={curKey}
             routerList={router} 
             currentMenu={mainMenuKey}
             changeSideMenu={this.changeSideMenu}
           />
         </Header>
         <Layout>
-          <Sider width={200} className="site-layout-background">
+          <Sider  className="page-sidebar" width={80}>
             <SideNav 
               routerList={sideMenuArr}  
-              key={mainMenuKey}
+              key={curKey}
+              onSelected={this.onSelected}
             />
           </Sider>
           <Layout style={{ padding: '0 24px 24px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
             <Content
-              className="site-layout-background"
-              style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280,
-              }}
+              className="page-wrapper"
             >
+               <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>{title}</Breadcrumb.Item>
+                
+            </Breadcrumb>
               {this.props.children}
             </Content>
           </Layout>
